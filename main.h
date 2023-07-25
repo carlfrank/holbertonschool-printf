e710a9109184dbe908459b8672b7b41058f29f0c
carlfrank <carl-frank7@hotmail.com>
2023-07-25 18:42:52 +0000

    main.h

--- main.h
+++ main.h
@@ -1,37 +1,18 @@
 #ifdef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
 /**
- * struct fmt - Struct op
- * by Carlos Carrasquillo and Juan Silva
- *
- * @fmt: The format.
- * @fn: The function associated.
+ * _printf - formated output functions.
+ * @format: character string
+ * Return: the number of character
  */
-struct fmt
-{
-  char fmt;
-  int (*fn)(va_list, char[], int, int, int, int);
-  void (*f)();
-};
 
-/**
- * typedef struct fmt fmt_t - Struct op
- *
+int _print(const char *format, ...);
+
+int print_char(char c);
+int print_str(char *s);
+int print_interger(int num);
+
+#endif

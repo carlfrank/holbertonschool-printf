99b2773bbad360116792813319d463a3828fdba0
Mizuinu30 <juansilva.dvm@gmail.com>
2023-07-27 17:51:09 +0000

    hotfix

--- _printf.c
+++ _printf.c
@@ -1,7 +1,61 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _strlen - lenght of a string
- * @s: pointer to string
+ * _printf - printf function
  *
- *
+ * @format: formatted string
+ * Return: The total number of outputted characters
+ */
+int _printf(const char *format, ...)
+{
+	int i, j, count, find;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+	count = 0;
+
+	for (i = 0; *(format + i); i++)
+	{
+		if (*(format + i) == '%')
+		{
+			if (*(format + i + 1) == '\0')
+				continue;
+			find = 0;
+
+			for (j = 0; j < 6; j++)
+			{
+				if (*(format + i + 1) == arguments[j].spec)
+				{
+					count += arguments[j].print(list);
+					find = 1;
+					format++;
+					break;
+				}
+			}
+
+			if (find != 1)
+			{
+				if (*(format + i + 1) == '%')
+				{
+					count += write(1, "%", 1);
+					format++;
+				}
+				else
+					count += write(1, (format + i), 1);
+			}
+
+		}
+		else
+		{
+			count += write(1, (format + i), 1);
+		}
+	}
+	va_end(list);
+
+	return (count);
+}
